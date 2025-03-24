8a11f74ed7c6469d9a6dd7b80ae332fe57fdaf6b
Your Name <email@something.com>
2025-03-24 11:06:25 +0530

    removed and renamed folder

--- main/miniproject-KEVIN/scripts/config.js
+++
@@ -1,3 +0,0 @@
-export const supabaseUrl = 'https://prkeusllhjfxxbovdqjk.supabase.co';
-export const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBya2V1c2xsaGpmeHhib3ZkcWprIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE1MjcyMTgsImV4cCI6MjA1NzEwMzIxOH0.IsHvMd3pslFAucd7aw0y7sMtfflI-hP_oYtfn_-f4nY';
-export const MONGO_URI = 'mongodb+srv://johnkulangara1077:bijaliya@healthifycluster.ingar.mongodb.net/health_platform?retryWrites=true&w=majority';
